e5f5b363bb400494dcb85e8f6b20e702025e43e2
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-10-08 20:07:25 -0400

    API Authentication

---
+++ api/src/modules/providers/infra/typeorm/entities/Provider.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('providers')
+class Provider {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  category_id: string;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Provider;
