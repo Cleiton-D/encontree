7ae27c223f2a2f97d264602fd5d3f1bdecff7ebe
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-10-20 22:20:27 -0400

    Add navigation

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const App = (): JSX.Element => {
+const SignIn = (): JSX.Element => {
   return (
     <View>
-      <Text>Hello Encontree</Text>
+      <Text>SignIn</Text>
     </View>
   );
 };
 
-export default App;
+export default SignIn;
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import AuthRoutes from './auth.routes';
+
+const Routes = (): JSX.Element => <AuthRoutes />;
+
+export default Routes;
