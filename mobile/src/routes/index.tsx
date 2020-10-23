ac346d61bf9a2d3328f129010a21ea116c9f6c4a
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-10-22 22:51:22 -0400

    create app Routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
 import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
 
-const Routes = (): JSX.Element => <AuthRoutes />;
+import { useAuth } from '../hooks/auth';
+
+const Routes = (): JSX.Element => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#999" />
+      </View>
+    );
+  }
+
+  return user ? <AppRoutes /> : <AuthRoutes />;
+};
 
 export default Routes;
