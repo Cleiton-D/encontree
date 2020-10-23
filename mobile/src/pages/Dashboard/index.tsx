b475952827d52642417578b2bcf3befd74fac039
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-10-22 22:51:07 -0400

    Create Dashboard page

---
+++ mobile/src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const Dashboard = (): JSX.Element => {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+};
+
+export default Dashboard;
