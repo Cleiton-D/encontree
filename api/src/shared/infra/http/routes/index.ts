5879a392780b0508ecd48155cdbf469c3abff771
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-10-23 23:30:50 -0400

    Change app routes

--- api/src/shared/infra/http/routes/index.ts
+++ api/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import providersRouter from '@modules/providers/infra/http/routes/providers.routes';
+import categoriesRouter from '@modules/providers/infra/http/routes/categories.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
@@ -10,6 +11,7 @@ const routes = Router();
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/providers', providersRouter);
+routes.use('/categories', categoriesRouter);
 routes.use('/profile', profileRouter);
 
 export default routes;
