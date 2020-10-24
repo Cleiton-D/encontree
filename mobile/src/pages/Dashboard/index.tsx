a8d2df93c3db3aa3b39dfdf5765f87f6617fc499
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-10-23 21:44:47 -0400

    Change Dashboard page

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Container, Text } from './styles';
 
 const Dashboard = (): JSX.Element => {
+  const { user } = useAuth();
+
   return (
     <Container>
       <Text>Dashboard</Text>
+      <Text>{user.username}</Text>
     </Container>
   );
 };
